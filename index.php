b8a50e8a148da3232099857c75c211a63d1cb795
QravaTTacker <cristianpessotto88@gmail.com>
2023-08-28 19:39:18 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// Code
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Cristian Pessotto" />
+    <meta name="description" content="Descrição" />
+    <meta name="keywords" content="HTML,  CSS,  JS" />
+    <link href="css/style.css" rel="stylesheet" />
+    <link href="icons/name.png" rel="icon" type="image/png" size="16x16" />
+    <title>My Web Site</title>
+    <style>
+        body {
+            background-color: #110f18;
+            color: #61ffca;
+        }
+    </style>
+    <script>
+    </script>
+</head>
+
+<body>
+    <!--! Cabeçalho da Página -->
+    <header>
+        <!--Code-->
+    </header>
+    <!--! Conteúdo da Página -->
+    <main>
+        <section></section>
+        <section></section>
+    </main>
+    <!--! Rodapé da Página -->
+    <footer></footer>
+</body>
+
+</html>
