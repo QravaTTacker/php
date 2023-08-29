61f797f9c1279b7f1e1e7a5b22d057c1abe54524
QravaTTacker <cristianpessotto88@gmail.com>
2023-08-28 21:20:43 -0300

    Initial commit

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="Cristian Pessotto" />
-    <meta name="description" content="Descrição" />
+    <meta name="description" content="Página inicial para incorporar todos os conteúdos discutidos e estudados em PHP" />
     <meta name="keywords" content="HTML,  CSS,  JS" />
-    <link href="css/style.css" rel="stylesheet" />
-    <link href="icons/name.png" rel="icon" type="image/png" size="16x16" />
+    <!-- <link href="css/style.css" rel="stylesheet" /> -->
+    <!-- <link href="icons/name.png" rel="icon" type="image/png" size="16x16" /> -->
     <title>My Web Site</title>
     <style>
         body {
@@ -19,8 +19,6 @@
             color: #61ffca;
         }
     </style>
-    <script>
-    </script>
 </head>
 
 <body>
@@ -30,7 +28,11 @@
     </header>
     <!--! Conteúdo da Página -->
     <main>
-        <section></section>
+        <section>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dignissimos molestias ullam asperiores vitae esse minima rem quo. Voluptate, fuga! Ea deserunt perspiciatis labore ducimus dolorum, esse libero ut quasi!
+            </p>
+        </section>
         <section></section>
     </main>
     <!--! Rodapé da Página -->
